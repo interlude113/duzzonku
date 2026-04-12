84e6afcc2619742558f36457fd735d37a32051ae
interlude113 <145578936+interlude113@users.noreply.github.com>
2026-04-12 12:09:40 +0900

    Delete Woori directory

--- Woori/Core/Repositories/Protocols/AnniversaryRepositoryProtocol.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-protocol AnniversaryRepositoryProtocol {
-    func fetchAll(coupleId: String) async throws -> [Anniversary]
-    func add(coupleId: String, anniversary: Anniversary) async throws
-    func update(coupleId: String, anniversary: Anniversary) async throws
-    func delete(coupleId: String, anniversaryId: String) async throws
-    func listenToAnniversaries(coupleId: String) -> AsyncStream<[Anniversary]>
-}
--- Woori/Core/Repositories/Protocols/LetterRepositoryProtocol.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-protocol LetterRepositoryProtocol {
-    func fetchAll(coupleId: String) async throws -> [Letter]
-    func add(coupleId: String, letter: Letter) async throws
-    func markAsRead(coupleId: String, letterId: String) async throws
-    func delete(coupleId: String, letterId: String) async throws
-    func listenToLetters(coupleId: String) -> AsyncStream<[Letter]>
-}
